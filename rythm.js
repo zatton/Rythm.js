56e57c60dcfb7d52821d740e3faf9517e5982adf
Benjamin Plouzennec <plouzennec.benjamin@gmail.com>
2019-06-11 10:43:45 +0200

    :sparkles: Font color (#113)
    * :lipstick: Some ui adjustement on demo, David goodenougth
    
    * font-color-dance
    
    * font-color-dance
    
    * translation README.md to Korean
    
    * translation README.md to Korean
    
    * Delete README_kr.md
    
    * :lipstick: Updated demo page style for fonts
    
    * :lipstick: Minor ui adjusment on demo page

--- rythm.js
+++ rythm.js
@@ -2,8 +2,8 @@
   typeof exports === 'object' && typeof module !== 'undefined'
     ? (module.exports = factory())
     : typeof define === 'function' && define.amd
-      ? define(factory)
-      : (global.Rythm = factory())
+    ? define(factory)
+    : (global.Rythm = factory())
 })(this, function() {
   'use strict'
 
